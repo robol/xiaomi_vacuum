7470aeeefd4352b27b8ba845ecca34be53437f18
Leonardo Robol <leo@robol.it>
2020-12-14 12:04:57 +0100

    Report state='cleaning' while zone cleaning.

--- custom_components/xiaomi_vacuum/vacuum.py
+++ custom_components/xiaomi_vacuum/vacuum.py
@@ -1,6 +1,6 @@
 """Xiaomi Vacuum"""
 from functools import partial
-import logging
+import logging, time
 import voluptuous as vol
 
 from .miio import DreameVacuum, DeviceException
@@ -178,8 +178,9 @@ class MiroboVacuum(StateVacuumEntity):
 
         self._total_clean_count = None
         self._cleaning_area = None
-        self._cleaning_time = None		
-
+        self._cleaning_time = None
+        
+        self._zone_cleaning_start = None
         
 
     @property
@@ -191,6 +192,17 @@ class MiroboVacuum(StateVacuumEntity):
     def state(self):
         """Return the status of the vacuum cleaner."""
         if self.vacuum_state is not None:
+            # We keep track of the last started zone cleaning, since the Vacuum
+            # reports the state as idle while zone cleaning, and we'd rather see
+            # "cleaning". The override is reset as soon as the state changes to
+            # anything different from 'idle', which is state 2. 
+            if self._zone_cleaning_start is not None:
+                if int(self.vacuum_state) == 2 or \
+                   time.time() < self._zone_cleaning_start + 30:
+                    return STATE_CLEANING
+                else:
+                    self._zone_cleaning_start = None
+
             try:
                 return STATE_CODE_TO_STATE[int(self.vacuum_state)]
             except KeyError:
@@ -241,7 +253,7 @@ class MiroboVacuum(StateVacuumEntity):
         """Return the specific state attributes of this vacuum cleaner."""
         if self.vacuum_state is not None:
             return {
-                ATTR_STATUS: STATE_CODE_TO_STATE[int(self.vacuum_state)],
+                ATTR_STATUS: self.state,
                 ATTR_ERROR:  ERROR_CODE_TO_ERROR.get(self.vacuum_error, "Unknown"),
 				ATTR_FAN_SPEED: SPEED_CODE_TO_NAME.get(self._current_fan_speed, "Unknown"),
                 ATTR_MAIN_BRUSH_LEFT_TIME: self._main_brush_time_left,
@@ -251,7 +263,7 @@ class MiroboVacuum(StateVacuumEntity):
                 ATTR_FILTER_LIFE_LEVEL: self._filter_life_level,
                 ATTR_FILTER_LEFT_TIME: self._filter_left_time,
                 ATTR_CLEANING_AREA: self._cleaning_area,
-                ATTR_CLEANING_TIME: self._cleaning_time,				
+                ATTR_CLEANING_TIME: self._cleaning_time,
                 ATTR_CLEANING_TOTAL_TIME: self._total_clean_count,
             } 
 
@@ -288,6 +300,7 @@ class MiroboVacuum(StateVacuumEntity):
         """Clean selected area."""
         try:
             await self.hass.async_add_executor_job(self._vacuum.zone_cleanup, zone)
+            self._zone_cleaning_start = time.time()
         except (OSError, DeviceException) as exc:
             _LOGGER.error("Unable to send zoned_clean command to the vacuum: %s", exc)
 
